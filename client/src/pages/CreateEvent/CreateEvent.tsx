31086fc5945509f84b403e0b52f7ca88365ae712
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2024-05-03 14:42:35 +0200

    Ajout du style

--- client/src/pages/CreateEvent/CreateEvent.tsx
+++ client/src/pages/CreateEvent/CreateEvent.tsx
@@ -47,51 +47,52 @@ function CreateEvent() {
   };
 
   return (
-    <div>
-      <h1>Créer un événement</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Titre de l'événement:
-          <input type="text" name="title" value={eventData.title} onChange={handleChange} />
-        </label>
-        <label>
-          Description de l'événement:
-          <textarea name="description" value={eventData.description} onChange={handleChange} />
-        </label>
-        <label>
-          Date de l'événement:
-          <input type="date" name="date" value={eventData.date} onChange={handleChange} />
-        </label>
-        <label>
-          Nombre de places :
-          <input type="number" name="nbPlaces" value={eventData.nbPlaces} onChange={handleChange} />
-        </label>
-        <label>
-          Adresse:
-          <input type="text" name="address" value={eventData.address} onChange={handleChange} />
-        </label>
-        <label>
-          Photos:
-          <input type="text" name="pictures" value={eventData.pictures} onChange={handleChange} />
-        </label>
-        <label>
-          Liens:
-          <input type="text" name="links" value={eventData.links} onChange={handleChange} />
-        </label>
-        <label>
-          Type d'événement:
-          <select name="categoryId" value={eventData.categoryId} onChange={handleChange}>
-            <option value="">Sélectionner un type d'événement</option>
-            {eventTypes.map((eventType) => (
-              <option key={eventType.id} value={eventType.id}>
-                {eventType.label}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <button type="submit">Valider</button>
-      </form>
+    <div className="bg-gray-600 min-h-screen flex justify-center items-center">
+      <div className="max-w-md w-full p-8 bg-gray-600 rounded-lg">
+        <h1 className="text-2xl mb-4 text-white">Créer un événement</h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex flex-col">
+            <label className="text-white">Titre de l'événement:</label>
+            <input type="text" name="title" value={eventData.title} onChange={handleChange} className="input bg-gray-100 rounded-md" />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white">Description de l'événement:</label>
+            <textarea name="description" value={eventData.description} onChange={handleChange} className="input bg-gray-100 rounded-md"></textarea>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white">Date de l'événement:</label>
+            <input type="date" name="date" value={eventData.date} onChange={handleChange} className="input bg-gray-100 rounded-md" />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white">Nombre de places :</label>
+            <input type="number" name="nbPlaces" value={eventData.nbPlaces} onChange={handleChange} className="input bg-gray-100 rounded-md" />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white">Adresse:</label>
+            <input type="text" name="address" value={eventData.address} onChange={handleChange} className="input bg-gray-100 rounded-md" />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white">Photos:</label>
+            <input type="text" name="pictures" value={eventData.pictures} onChange={handleChange} className="input bg-gray-100 rounded-md" />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white">Liens:</label>
+            <input type="text" name="links" value={eventData.links} onChange={handleChange} className="input bg-gray-100 rounded-md" />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-white">Type d'événement:</label>
+            <select name="categoryId" value={eventData.categoryId} onChange={handleChange} className="input bg-gray-100 rounded-md">
+              <option value="">Sélectionner un type d'événement</option>
+              {eventTypes.map((eventType) => (
+                <option key={eventType.id} value={eventType.id}>
+                  {eventType.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button type="submit" className="btn bg-blue-500 text-white rounded-md w-full float-left">Valider</button>
+        </form>
+      </div>
     </div>
   );
 }
